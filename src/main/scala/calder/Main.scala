d239647ff8d3e8e954292aad6cc17635ab7ccaf3
Andrew R. McBurney <andrewrobertmcburney@gmail.com>
2017-12-20 23:05:17 -0500

    Added MIT licence to Main.scala

--- src/main/scala/calder/Main.scala
+++ src/main/scala/calder/Main.scala
@@ -1,8 +1,29 @@
 /**
-  * Main.scala
-  *
-  * @version: 1.0
-  */
+ * Calder - A library for safely constructing and integrating WebGL shaders
+ *
+ * Copyright (c) 2017 Paul Bardea, Tammy Liu, Abhishek Madan,
+ *   Andrew Robert McBurney, and Dave Pagurek Van Mossel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @version: 1.0
+ */
 
 package calder
 
