7d88e712348ea7660081863a75bd87e7d7c43e58
Andrew R. McBurney <andrewrobertmcburney@gmail.com>
2017-12-23 12:38:46 -0500

    Added ShaderPipelineBuilder.scala.

---
+++ src/main/scala/calder/shaders/ShaderPipelineBuilder.scala
@@ -0,0 +1,22 @@
+/**
+ * ShaderPipelineBuilder.scala
+ */
+
+package calder.shaders
+
+import calder.shaders.Shader
+import calder.shaders.ShaderPipeline
+import calder.variables.VariableSource
+
+import org.scalajs.dom.raw.WebGLRenderingContext
+
+class ShaderPipelineBuilder(private val vertexShader: Shader, private val fragmentShader: Shader) {
+  private val inputVariables: Set[VariableSource]  = fragmentShader.inputs
+  private val outputVariables: Set[VariableSource] = vertexShader.outputs
+
+  /** Public Interface */
+
+  val isWellFormed: Boolean = inputVariables.subsetOf(outputVariables)
+
+  def build(gl: WebGLRenderingContext): ShaderPipeline = new ShaderPipeline(gl, vertexShader, fragmentShader)
+}
