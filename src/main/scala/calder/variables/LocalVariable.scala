dd748e8e2ca1904f42d801215cc7261efeabb872
Andrew R. McBurney <andrewrobertmcburney@gmail.com>
2017-12-23 12:46:24 -0500

    Added LocalVariable.scala

---
+++ src/main/scala/calder/variables/LocalVariable.scala
@@ -0,0 +1,10 @@
+/**
+ * LocalVariable.scala
+ */
+
+package calder.variables
+
+import calder.variables.Variable
+import calder.variables.VariableSource
+
+class LocalVariable(private val variableSrc: VariableSource) extends Variable(variableSrc) {}
