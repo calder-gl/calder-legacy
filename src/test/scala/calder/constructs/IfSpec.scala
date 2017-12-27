46893266c8b1746432a7aa5ace9bd235ee6b7a68
Andrew R. McBurney <andrewrobertmcburney@gmail.com>
2017-12-27 13:14:23 -0500

    Moved IfSpec to construct test suite.

--- src/test/scala/calder/constructs/IfSpec.scala
+++ src/test/scala/calder/constructs/IfSpec.scala
@@ -13,12 +13,18 @@ import calder.expressions.constructs._
 import calder.types._
 import calder.variables._
 
-class IfSpec extends FunSpec with Matchers {
-  describe ("If") {
-    val a = new Reference(new InterfaceVariable(Qualifier.In, new VariableSource(Kind.Bool.asInstanceOf[Type], "a")))
-    val b = new Reference(new InterfaceVariable(Qualifier.In, new VariableSource(Kind.Bool.asInstanceOf[Type], "b")))
-    val c = new Reference(new InterfaceVariable(Qualifier.In, new VariableSource(Kind.Bool.asInstanceOf[Type], "c")))
+class IfSpec extends FunSpec {
+  val a = new Reference(
+    new InterfaceVariable(Qualifier.In, new VariableSource(Kind.Bool.asInstanceOf[Type], "a"))
+  )
+  val b = new Reference(
+    new InterfaceVariable(Qualifier.In, new VariableSource(Kind.Bool.asInstanceOf[Type], "b"))
+  )
+  val c = new Reference(
+    new InterfaceVariable(Qualifier.In, new VariableSource(Kind.Int.asInstanceOf[Type], "intVar"))
+  )
 
+  describe ("If") {
     describe ("source") {
       it ("has no else block if none is provided") {
         val ifStatement = new If(a, new Block(Array(new Statement(new EqualAssignment(a, b)))))
@@ -26,11 +32,11 @@ class IfSpec extends FunSpec with Matchers {
       }
 
       it ("has no else block if an empty block is provided") {
-        val ifStatement = new If(a, new Block(Array(new Statement(new EqualAssignment(a, b)))), new Block(Array()))
+        val ifStatement = new If(a, new Block(Array(new Statement(new EqualAssignment(a, b)))), new Block())
         assert(ifStatement.source == "if (a) { a=b; }")
       }
 
-      it ("has else block provided") {
+      it ("has else block if non-empty else block provided") {
         val ifStatement = new If(
           a,
           new Block(Array(new Statement(new EqualAssignment(a, b)))),
